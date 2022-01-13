3c99ecada03769d78cc711a358f34d149da83b9a
ph4s3r <spec1al@vipmail.hu>
2022-01-13 11:14:23 +0100

    Update ftpsetup.sh

--- ftpsetup.sh
+++ ftpsetup.sh
@@ -31,9 +31,20 @@ else
   sudo service vsftpd start
   sudo service vsftpd status
 fi
+
+
 printf "${GREEN}connect to the ftp server and download directories with
 ${RED}wget -r -nH --cut-dirs=1 -nc ftp://gomboc:passwordhere@uvm.westeurope.cloudapp.azure.com/dev/pml_data
 ${GREEN}where
 -nc: ignoring files that already exist at destination (even if the file is different)
 -nH: Is for disabling creation of directory having name same as URL i.e. abc.xyz.com
 --cut-dirs: Is for ignoring number of parent directories. The value of this option will differ for your command."
+
+printf " Upload dirs with 
+${RED}ncftpput -avR -u <user> -p<password> <ftpserver> <path to put> <local folder>
+${GREEN}where,
+
+a transfer all files in ASCII mode and not in binary.
+v to see output in verbose mode
+R to copy folders in recursive mode.
+"
