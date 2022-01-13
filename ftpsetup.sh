e8e0e7df226c02f9390c6f8ddc6a677e5f76a596
ph4s3r <spec1al@vipmail.hu>
2022-01-13 10:31:30 +0100

    Update ftpsetup.sh

--- ftpsetup.sh
+++ ftpsetup.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-echo "Hi, please enter the external ip address to use (yes it is needed..) :"
+pidof systemd && sd="TRUE" || sd="FALSE"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -n "Hi, please enter the external ip address to use (yes it is needed..) :"
 
 read ip
 
-echo "Working... Please don`t forget to open ports 20,21 and 30000->30100 for passive mode"
-
-sudo apt install -y vsftpd
-
+echo "Working... ${RED}Please dont forget to open ports 20,21 and 30000->30100 for passive mode${NC}"
+sudo apt install -y -qq vsftpd
 # Making a backup of original config
-
 sudo cp /etc/vsftpd.conf  /etc/vsftpd.conf_default
 
-vim /etc/vsftpd.conf
-
+sudo echo "
 seccomp_sandbox=NO
 allow_writeable_chroot=YES
 pasv_address=$ip
@@ -23,18 +24,15 @@ pasv_max_port=30100
 port_enable=YES
 write_enable=YES
 listen_ipv6=NO
-listen=YES
-
-:wq
-
+listen=YES" | sudo tee -a /etc/vsftpd.conf > /dev/null
 # Launch and enable
-
-sudo systemctl start vsftpd
-
-sudo systemctl enable vsftpd
-
-sudo systemctl restart vsftpd
-
-echo "connect to the ftp server and download directories with wget -r -nH --cut-dirs=1 -nc ftp://gomboc:passwordhere@uvm.westeurope.cloudapp.azure.com/dev/pml_data/"
-
-sudo systemctl status vsftpd
+if [ $sd = "TRUE" ]
+then
+  sudo systemctl start vsftpd
+  sudo systemctl enable vsftpd
+  sudo systemctl status vsftpd
+else
+  sudo service vsftpd start
+  sudo service vsftpd status
+fi
+printf "${GREEN}connect to the ftp server and download directories with wget -r -nH --cut-dirs=1 -nc ftp://gomboc:passwordhere@uvm.westeurope.cloudapp.azure.com/dev/pml_data"
